80ed03a84b0b541400aa261cb83556a656030233
GEOEGII555 <geoegii2001555@gmail.com>
2023-05-23 10:50:26 +0300

    Update update.bat file

--- update.bat
+++ update.bat
@@ -3,3 +3,5 @@ set /P "updates=What have you changed? "
 git add .
 git commit --allow-empty --allow-empty-message -m "%updates%"
 git push
+echo Now wait until it finishes building. It takes like 15-30 seconds
+pause
